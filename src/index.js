d849d5c63187df76d57aff043f6d1e41ced35e5d
Axel Simón <asimon@gisce.net>
2017-02-23 18:57:38 +0100

    Replaced "hello world" by Main component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Main from './components/Main'
 
 injectTapEventPlugin();
 
+const App = () => (
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Main />
+    </MuiThemeProvider>
+);
+
 ReactDOM.render(
-    <div>
-        <h1>Hola món!</h1>
-    </div>,
+    <App/>,
     document.getElementById('root')
 );
