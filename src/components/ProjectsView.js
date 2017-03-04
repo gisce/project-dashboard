217e40274db4fa2ae7e6ae7e5ae170ee41a31f72
Axel Simón <asimon@gisce.net>
2017-03-04 19:49:04 +0100

    Code cleanup for ProjectsView

--- src/components/ProjectsView.js
+++ src/components/ProjectsView.js
@@ -1,161 +1,93 @@
-import React from 'react';
-import {Table, TableBody, TableHeader, TableHeaderColumn } from 'material-ui/Table'
+import React, { Component } from 'react';
+import {Table, TableBody, TableHeader, TableRow, TableHeaderColumn, TableFooter } from 'material-ui/Table'
 import Project from './Project'
-
-import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-import MenuItem from 'material-ui/MenuItem';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MainPaper from './MainPaper'
+import MainMenu from './MainMenu'
 import PowERP from '../api/PowERP.js'
 
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+function GetProjects() {
+    /**
+     * This function fetches all projects from PowERP server
+     * using PowERP api.
+     * */
+    var api = new PowERP();
+    let projects = [];
+    let project_ids = api.search("project.project", []);
+    for(let i = 0; i < project_ids.length; i++){
+        let project = api.read("project.project", project_ids[i], ['id', 'title', 'subtitle', 'avatar', 'description']);
+        projects.push(project);
+    }
+    return {
+        "projects": projects
+    }
+}
 
-// const styles = {
-//     container: {
-//         marginTop: 75,
-//     },
-//     project: {
-//         marginTop: 10,
-//     },
-//     appbar: {
-//         position: 'fixed',
-//         top: 0,
-//         left: 0
-//     }
-// };
+const style = {
+    paperStyle: {
+        marginLeft: 180,
+        marginRight: 0,
+        width: '85%'
+    },
+    viewStyle: {
+        height: '85%'
+    },
+    tableStyle: {
+        height: '85%'
+    }
+};
 
-// const AppBarMenu = (props) => (
-//     <IconMenu
-//         {...props}
-//         iconButtonElement={
-//             <IconButton><MoreVertIcon /></IconButton>
-//         }
-//         targetOrigin={{horizontal: 'right', vertical: 'top'}}
-//         anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-//     >
-//         <MenuItem primaryText="Configuració" />
-//         <MenuItem primaryText="Ajuda" />
-//         <MenuItem primaryText="Sortir" />
-//     </IconMenu>
-// );
-//
-// AppBarMenu.muiName = 'IconMenu';
-//
-// function GetProjects() {
-//     /**
-//      * This function fetches all projects from PowERP server
-//      * using PowERP api.
-//      * */
-//     var api = new PowERP();
-//     let projects = [];
-//     let project_ids = api.search("project.project", []);
-//     for(let i = 0; i < project_ids.length; i++){
-//         let project = api.read("project.project", project_ids[i], ['id', 'title', 'subtitle', 'avatar', 'description']);
-//         projects.push(project);
-//     }
-//     return {
-//         "projects": projects
-//     }
-// }
 
-// function Project(props) {
-//     /*
-//      * This function returns a Card project component.
-//      * Properties must be provided.
-//      * */
-//     return (
-//         <Card>
-//             <CardHeader
-//                 title={props.title}
-//                 subtitle={props.subtitle}
-//                 avatar={props.avatar}
-//                 actAsExpander={true}
-//                 showExpandableButton={true}
-//             />
-//             <CardActions>
-//                 <FlatButton label="Tasques" />
-//             </CardActions>
-//             <CardText expandable={true}>
-//                 {props.description}
-//             </CardText>
-//         </Card>
-//     )
-// }
-
-export default class ProjectsView extends React.Component {
+export default class ProjectsView extends Component {
     render() {
+        let bundle = [];
+        let pr = GetProjects();
+        let projects = pr.projects;
+        for (let i = 0; i < projects.length; i++){
+            bundle.push(
+                <Project
+                    key={i}
+                    title={projects[i].title}
+                    subtitle="Subtítol"
+                    avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
+                    description="Descripció"
+                />
+            )
+        }
         return(
-            <Table>
-                <TableBody>
-                    <Project
-                        title="Títol"
-                        subtitle="Subtítol"
-                        avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
-                        description="Descripció"
-                    />
-                    <Project
-                        title="Títol"
-                        subtitle="Subtítol"
-                        avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
-                        description="Descripció"
-                    />
-                    <Project
-                        title="Títol"
-                        subtitle="Subtítol"
-                        avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
-                        description="Descripció"
-                    />
-                    <Project
-                        title="Títol"
-                        subtitle="Subtítol"
-                        avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
-                        description="Descripció"
-                    />
-                    <Project
-                        title="Títol"
-                        subtitle="Subtítol"
-                        avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
-                        description="Descripció"
-                    />
-                </TableBody>
-            </Table>
+            <div style={style.viewStyle}>
+                <div>
+                    <MainMenu/>
+                </div>
+                <div style={style.paperStyle}>
+                    <MainPaper>
+                        <Table height={'92%'}>
+                            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                                <TableRow>
+                                    <TableHeaderColumn>Avatar</TableHeaderColumn>
+                                    <TableHeaderColumn>Títol</TableHeaderColumn>
+                                    <TableHeaderColumn>Subtítol</TableHeaderColumn>
+                                    <TableHeaderColumn>Descripció</TableHeaderColumn>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody stripedRows={true} showRowHover={true}>
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                                {bundle}
+                            </TableBody>
+                            <TableFooter/>
+                        </Table>
+                    </MainPaper>
+                </div>
+            </div>
         )
     }
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         bundle: []
-    //     };
-    // }
-    // render() {
-    //     let pr = GetProjects();
-    //     let projects = pr.projects;
-    //     for (let i = 0; i < pr.projects.length; i++){
-    //         this.state.bundle.push(
-    //             <div key={i} style={styles.project}>
-    //                 <Project
-    //                     key={i}
-    //                     index={i}
-    //                     title={projects[i].title}
-    //                     subtitle={projects[i].subtitle}
-    //                     avatar={projects[i].avatar}
-    //                     description={projects[i].description}
-    //                 />
-    //             </div>
-    //         )
-    //     }
-    //     return (
-    //         <div>
-    //             <AppBar
-    //                 title = "Projectes"
-    //                 iconElementRight={<AppBarMenu/>}
-    //                 style={styles.appbar}/>
-    //             <div style={styles.container}>
-    //                 {this.state.bundle}
-    //             </div>
-    //         </div>
-    //     );
-    // }
 }
