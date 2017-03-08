e64cded140f62933ff47482e9c278c1111d2725c
Axel Simón <asimon@gisce.net>
2017-03-08 17:28:26 +0100

    Middleware support for Redux Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,36 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers'
+import thunkMiddleware  from 'redux-thunk'
+
+
+const debugware = [];
+if (process.env.NODE_ENV !== 'production') {
+    const createLogger = require('redux-logger');
+
+    debugware.push(createLogger({
+        collapsed: true,
+    }));
+}
 
 export default function configureStore(initialState) {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
     const store = createStore(
         rootReducer,
-        initialState
+        initialState,
+        composeEnhancers(
+            applyMiddleware(thunkMiddleware, ...debugware)
+        )
     );
+
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers/index').default;
+
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
     return store;
 }
