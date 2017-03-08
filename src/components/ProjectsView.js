09772c106b29a80606079e7d3bb749dd098427de
Axel Simón <asimon@gisce.net>
2017-03-08 17:30:21 +0100

    ProjectsView refactored

--- src/components/ProjectsView.js
+++ src/components/ProjectsView.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actionCreators from '../actions/projects';
 import {Table, TableBody, TableHeader, TableRow, TableHeaderColumn, TableFooter } from 'material-ui/Table'
 import Project from './Project'
 import MainPaper from './MainPaper'
 import MainMenu from './MainMenu'
 import PowERP from '../api/PowERP.js'
 
-function GetProjects() {
-    /**
-     * This function fetches all projects from PowERP server
-     * using PowERP api.
-     * */
-    var api = new PowERP();
-    let projects = [];
-    let project_ids = api.search("project.project", []);
-    for(let i = 0; i < project_ids.length; i++){
-        let project = api.read("project.project", project_ids[i], ['id', 'title', 'subtitle', 'avatar', 'description']);
-        projects.push(project);
-    }
+// function GetProjects() {
+//     /**
+//      * This function fetches all projects from PowERP server
+//      * using PowERP api.
+//      * */
+//     var api = new PowERP();
+//     let projects = [];
+//     let project_ids = api.search("project.project", []);
+//     for(let i = 0; i < project_ids.length; i++){
+//         let project = api.read("project.project", project_ids[i], ['id', 'title', 'subtitle', 'avatar', 'description']);
+//         projects.push(project);
+//     }
+//     return {
+//         "projects": projects
+//     }
+// }
+
+function mapStateToProps(state) {
     return {
-        "projects": projects
-    }
+        data: state.projects,
+        token: null,
+        loaded: state.projects.loaded,
+        isFetching: state.projects.isFetching,
+        message_text: state.projects.message_text,
+    };
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
 
 const style = {
     paperStyle: {
@@ -36,22 +54,42 @@ const style = {
     }
 };
 
-
+@connect(mapStateToProps, mapDispatchToProps)
 export default class ProjectsView extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            message_text: null
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData(initial = true) {
+        const token = "FOO"
+        // must replace by real token
+        this.props.fetchProjects(token, initial);
+    }
+
     render() {
         let bundle = [];
-        let pr = GetProjects();
-        let projects = pr.projects;
-        for (let i = 0; i < projects.length; i++){
-            bundle.push(
-                <Project
-                    key={i}
-                    title={projects[i].title}
-                    subtitle="Subtítol"
-                    avatar="https://avatars2.githubusercontent.com/u/13195695?v=3&s=460"
-                    description="Descripció"
-                />
-            )
+        // let pr = GetProjects();
+        // let projects = pr.projects;
+        if (this.props.loaded) {
+            let projectes = JSON.parse(JSON.stringify(this.props.data.data));
+            for (let i = 0; i < this.props.data.data.length; i++){
+                bundle.push(
+                    <Project
+                        key={i}
+                        title={projectes[i].title}
+                        partner={projectes[i].partner}
+                        avatar={projectes[i].avatar}
+                        state={projectes[i].state}
+                    />
+                )
+            }
         }
         return(
             <div style={style.viewStyle}>
@@ -60,31 +98,27 @@ export default class ProjectsView extends Component {
                 </div>
                 <div style={style.paperStyle}>
                     <MainPaper>
-                        <Table height={'92%'}>
-                            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-                                <TableRow>
-                                    <TableHeaderColumn>Avatar</TableHeaderColumn>
-                                    <TableHeaderColumn>Títol</TableHeaderColumn>
-                                    <TableHeaderColumn>Subtítol</TableHeaderColumn>
-                                    <TableHeaderColumn>Descripció</TableHeaderColumn>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody stripedRows={true} showRowHover={true}>
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                                {bundle}
-                            </TableBody>
-                            <TableFooter/>
-                        </Table>
+                        {
+                            this.props.loaded ?
+                            <Table height={'92%'}>
+                                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                                    <TableRow>
+                                        <TableHeaderColumn>Avatar</TableHeaderColumn>
+                                        <TableHeaderColumn>Títol</TableHeaderColumn>
+                                        <TableHeaderColumn>Responsable</TableHeaderColumn>
+                                        <TableHeaderColumn>Estat</TableHeaderColumn>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody stripedRows={true} showRowHover={true}>
+                                    {bundle}
+                                </TableBody>
+                                <TableFooter/>
+                            </Table>
+                        :
+                                <div>
+                                    No hi ha projectes per mostrar.
+                                </div>
+                        }
                     </MainPaper>
                 </div>
             </div>
