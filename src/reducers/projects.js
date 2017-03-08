560ab2769668871ae425c5268c7771430a6e9f50
Axel Simón <asimon@gisce.net>
2017-03-08 17:22:24 +0100

    Project's reducers added

---
+++ src/reducers/projects.js
@@ -0,0 +1,23 @@
+import { RECEIVE_PROJECTS, FETCH_PROJECTS_REQUEST } from '../constants';
+import { createReducer } from '../utils/misc';
+
+const initialState = {
+    data: null,
+    isFetching: false,
+    loaded: false,
+};
+
+export default createReducer(initialState, {
+    [RECEIVE_PROJECTS]: (state, payload) =>
+        Object.assign({}, state, {
+            data: payload.data,
+            isFetching: false,
+            loaded: true,
+            message_text: payload.message,
+        }),
+    [FETCH_PROJECTS_REQUEST]: (state, payload) =>
+        Object.assign({}, state, {
+            isFetching: true,
+            message_text: payload.message,
+        }),
+});
