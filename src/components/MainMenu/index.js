37c48c84b26eac1eee4aa0ea7d5fff26d9bc621f
Axel Simón <asimon@gisce.net>
2017-03-11 12:53:30 +0100

    Button click support added for menu

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -3,12 +3,13 @@ import Menu from 'material-ui/Menu';
 import Paper from 'material-ui/Paper';
 import MenuItem from 'material-ui/MenuItem';
 import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye';
+import { redirectToRoute } from '../../utils/http_functions'
 
 const style = {
     paper: {
         display: 'inline-block',
-        float: 'left',
-        height: '100%'
+        height: '93.1%',
+        width: '224px'
     }
 };
 
@@ -20,7 +21,7 @@ export default class MainMenu extends React.Component {
                     <Menu>
                         <MenuItem primaryText="Timer" leftIcon={<RemoveRedEye />} />
                         <MenuItem primaryText="Dashboard" leftIcon={<RemoveRedEye />} />
-                        <MenuItem primaryText="Projectes" leftIcon={<RemoveRedEye />} />
+                        <MenuItem primaryText="Projectes" onTouchTap={this.GoToProjects} leftIcon={<RemoveRedEye />} />
                         <MenuItem primaryText="Usuaris" leftIcon={<RemoveRedEye />} />
                         <MenuItem primaryText="Empreses" leftIcon={<RemoveRedEye />} />
                         <MenuItem primaryText="Configuració"/>
@@ -28,5 +29,9 @@ export default class MainMenu extends React.Component {
                 </Paper>
             </div>
         );
+    }
+
+    GoToProjects() {
+        redirectToRoute("/projects");
     }
 }
