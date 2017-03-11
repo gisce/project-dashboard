fb2b4ff4f8cf0382b15eb38b4ae22d490468d770
Axel Simón <asimon@gisce.net>
2017-03-11 12:55:31 +0100

    Project now can fetch tasks when is clicked

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import {TableRow, TableRowColumn} from 'material-ui/Table';
 import Avatar from 'material-ui/Avatar'
+import * as actionCreators from '../../actions/tasks';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+function mapStateToProps(state) {
+    return {
+        data: state.tasks,
+        token: null,
+        loaded: state.tasks.loaded,
+        isFetching: state.tasks.isFetching,
+        message_text: state.tasks.message_text,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
 
 const style = {margin: 5};
 
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
 
     render(){
-        const {avatar, title, partner, state, ...otherProps} = this.props;
+        const {avatar, title, partner, state, tasks, ...otherProps} = this.props;
         return(
-            <TableRow {...otherProps}>
+            <TableRow {...otherProps} onCellClick={this.onClick}>
                 <TableRowColumn>
                     <Avatar
                         src={avatar}
@@ -22,5 +45,11 @@ export default class Project extends Component {
                 <TableRowColumn>{state}</TableRowColumn>
             </TableRow>
         )
+    }
+
+    onClick() {
+        let token = "FOO";
+        let tasks = JSON.stringify(this.props.tasks);
+        this.props.fetchTasks(token, tasks, false);
     }
 }
