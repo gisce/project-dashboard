22791f3f113bd91c7234a2fb7d7903e88b303821
Axel Simón <asimon@gisce.net>
2017-03-11 12:52:30 +0100

    New style for appbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 
+const style = {
+    appBar: {
+        display: 'flex'
+
+    }
+
+};
+
 export class Header extends Component {
     render(){
         return(
             <header>
-                <AppBar
+                <AppBar style={style.appBar}
                     title="Project-Dashboard"
                 />
             </header>
