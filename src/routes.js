5c7d6b08941a929df6ed53607acdc6d75d657f30
Axel Simón <asimon@gisce.net>
2017-03-18 09:56:33 +0100

    New task route

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router';
 
 import { App } from './containers/App';
 import Projects from './components/ProjectsView';
-import Tasks from './components/TasksView'
+import Tasks from './components/TasksView';
+import Task from './components/TaskView';
 
 export default (
     <Route path="/" component={App}>
         <Route path="projects" component={Projects} />
         <Route path="tasks" component={Tasks} />
+        <Route path="task" component={Task} />
     </Route>
 );
