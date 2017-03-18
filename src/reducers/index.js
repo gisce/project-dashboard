32b033ee6be4c1efd7ad94eae70e65655a0aaa00
Axel Simón <asimon@gisce.net>
2017-03-18 09:56:52 +0100

    new taskWork combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import project from './project'
 import projects from './projects'
 import tasks from './tasks'
+import taskWork from './task_work'
 import search from './search'
 
 const appReducer = combineReducers({
     project,
     projects,
     tasks,
+    taskWork,
     search
 });
 
