f3ebb1b8bebb7b56b88bb3bd8af45198c1306616
Axel Simón <asimon@gisce.net>
2017-03-18 12:36:59 +0100

    Code cleanup for project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -30,8 +30,7 @@ export default class Project extends Component {
     }
 
     render(){
-        const {avatar, title, partner, status,
-            tasks, token, loaded, isFetching,
+        const {project, token, loaded, isFetching,
             message_text, fetchTasksRequest,
             receiveTasks, fetchTasks,
             ...otherProps} = this.props;
@@ -39,21 +38,21 @@ export default class Project extends Component {
             <TableRow {...otherProps} onCellClick={this.onClick}>
                 <TableRowColumn>
                     <Avatar
-                        src={avatar}
+                        src={project.avatar}
                         size={30}
                         style={style}
                     />
                 </TableRowColumn>
-                <TableRowColumn>{title}</TableRowColumn>
-                <TableRowColumn>{partner}</TableRowColumn>
-                <TableRowColumn>{status}</TableRowColumn>
+                <TableRowColumn>{project.title}</TableRowColumn>
+                <TableRowColumn>{project.partner}</TableRowColumn>
+                <TableRowColumn>{project.status}</TableRowColumn>
             </TableRow>
         )
     }
 
     onClick() {
         let token = "FOO";
-        let tasks = JSON.stringify(this.props.tasks);
+        let tasks = JSON.stringify(this.props.project.tasks);
         this.props.fetchTasks(token, tasks, false);
     }
 }
