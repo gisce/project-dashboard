7614de70990734a5c0ffc6b5b6dd32cb60d48b5b
Axel Simón <asimon@gisce.net>
2017-03-18 09:51:31 +0100

    New taskwork reducer added

---
+++ src/reducers/task_work.js
@@ -0,0 +1,23 @@
+import { RECEIVE_TASK_WORK, FETCH_TASK_WORK_REQUEST } from '../constants';
+import { createReducer } from '../utils/misc';
+
+const initialState = {
+    data: null,
+    isFetching: false,
+    loaded: false,
+};
+
+export default createReducer(initialState, {
+    [RECEIVE_TASK_WORK]: (state, payload) =>
+        Object.assign({}, state, {
+            data: payload,
+            isFetching: false,
+            loaded: true,
+            message_text: payload.message,
+        }),
+    [FETCH_TASK_WORK_REQUEST]: (state, payload) =>
+        Object.assign({}, state, {
+            isFetching: true,
+            message_text: payload.message,
+        }),
+});
