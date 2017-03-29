0519f0096f0657777e5e0994aeb1e78c3bde7c68
Axel Simón <asimon@gisce.net>
2017-03-29 20:09:29 +0200

    UI reducer has been combined

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import projects from './projects'
 import tasks from './tasks'
 import taskWorks from './task_work'
 import search from './search'
+import ui from './ui'
 
 const appReducer = combineReducers({
     projects,
     tasks,
     taskWorks,
-    search
+    search,
+    ui
 });
 
 const rootReducer = (state, action) => {
