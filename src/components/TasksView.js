88b3a03a46ab3c151957cb50271fce4580dda426
Axel Simón <asimon@gisce.net>
2017-04-03 16:27:45 +0200

    TasksView main container is now made with views

--- src/components/TasksView.js
+++ src/components/TasksView.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../actions/projects';
 import MainPaper from './MainPaper'
-import MainMenu from './MainMenu'
 import TaskList from './TaskList'
 import SearchBox from './SearchBox';
 
@@ -22,17 +21,6 @@ function mapDispatchToProps(dispatch) {
 }
 
 const estils = {
-    menuContainer: {
-        padding: 0,
-        margin: 0
-    },
-    paperContainer: {
-        width: '100%',
-        paddingLeft: 33,
-        paddingRight: 30,
-        paddingTop: 80,
-        margin: 0
-    },
     titol: {
         paddingLeft: 20,
         fontSize: 26,
@@ -41,19 +29,14 @@ const estils = {
     mainTable: {
         marginBottom: '20px'
     },
-    paperSectionStyle: {
-        height: "100%"
-    },
-    containerTable: {
-        height: "93.1%",
-        width: "100%",
-        position: "relative",
-        top: -3,
-        left: -3
-    },
     subtitol: {
         paddingLeft: 20,
         fontSize: 14
+    },
+    container: {
+        paddingLeft: "15%",
+        paddingRight: "15%",
+        paddingTop: 120
     },
 };
 
@@ -77,51 +60,40 @@ export default class TasksView extends Component {
             }
         }
         return(
-            <table style={estils.containerTable}>
-                <tr>
-                    <td style={estils.menuContainer}>
-                        <div style={{height: "100%", width: '100%', float: 'left'}}>
-                            <MainMenu/>
-                        </div>
-                    </td>
-                    <td className="paperSection" style={estils.paperContainer}>
-                        <div style={estils.paperSectionStyle}>
-                            <MainPaper>
-                                <table style={estils.mainTable}>
-                                    <tbody>
-                                    <tr>
-                                        <td style={estils.titol}>
-                                            Tasques
-                                        </td>
-                                        <td>
-                                            <SearchBox original_ids={tasks_ids} model="tasks"/>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style={estils.subtitol}>
-                                            {
-                                                project != "" ?
-                                                    <div>Projecte: {project}</div>
-                                                    :
-                                                    <div></div>
-                                            }
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+            <div style={estils.container}>
+                <MainPaper>
+                    <table style={estils.mainTable}>
+                        <tbody>
+                        <tr>
+                            <td style={estils.titol}>
+                                Tasques
+                            </td>
+                            <td>
+                                <SearchBox original_ids={tasks_ids} model="tasks"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={estils.subtitol}>
                                 {
-                                    this.props.loaded ?
-                                        <TaskList tasks={this.props.data.data.tasks} />
+                                    project != "" ?
+                                        <div>Projecte: {project}</div>
                                         :
-                                        <div style={{padding: 30}}>
-                                            No hi ha tasques per mostrar.
-                                        </div>
+                                        <div></div>
                                 }
-                            </MainPaper>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    {
+                        this.props.loaded ?
+                            <TaskList tasks={this.props.data.data.tasks} />
+                            :
+                            <div style={{padding: 30}}>
+                                No hi ha tasques per mostrar.
+                            </div>
+                    }
+                </MainPaper>
+            </div>
         )
     }
 }
