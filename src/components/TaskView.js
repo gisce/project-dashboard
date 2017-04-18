04c774e58b180827df1dad4f17519a16f6b5bf3f
Axel Simón <asimon@gisce.net>
2017-04-18 16:58:42 +0200

    TaskView now uses generic MainView and List components

--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
 import TextField from 'material-ui/TextField';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as actionCreators from '../actions/projects';
-import MainPaper from './MainPaper'
-import TaskWorkList from './TaskWorkList'
+import * as actionCreators from '../actions/task_work';
+import MainView from './MainView'
+import List from './List'
+import TaskWork from './TaskWork'
 
 function mapStateToProps(state) {
     let task = null;
@@ -32,34 +31,6 @@ function mapDispatchToProps(dispatch) {
     return bindActionCreators(actionCreators, dispatch);
 }
 
-const estils = {
-    container: {
-        paddingLeft: "15%",
-        paddingRight: "15%",
-        paddingTop: 120
-    },
-    titol: {
-        paddingTop: 20,
-        paddingLeft: 20,
-        fontSize: 26,
-        width: '70%'
-    },
-    subtitol: {
-        paddingLeft: 20,
-        paddingTop: 20,
-        fontSize: 14
-    },
-    mainTable: {
-        width: "100%",
-        marginBottom: '20px'
-    },
-    botoNou: {
-        float: 'right',
-        position: 'relative',
-        right: 5
-    }
-};
-
 @connect(mapStateToProps, mapDispatchToProps)
 export default class TasksView extends Component {
     constructor(props){
@@ -68,77 +39,52 @@ export default class TasksView extends Component {
             message_text: null
         };
     }
-
     render() {
+        let project = '';
+        let title = 'Tasca';
+        let continguts = [];
+        let cols = [
+            "Data",
+            "Realitzar per",
+            "Temps dedicat",
+            "Resum del treball",
+            ""
+        ];
+        let tableContents = "No s'ha seleccionat cap tasca.";
+        if(this.props.task) {
+            project = this.props.task.project;
+            title = this.props.task.description;
+            let workdones = this.props.taskWorks.data.taskWorks;
+            tableContents = workdones.map(task =>
+                <TaskWork
+                    key={task.id}
+                    taskWork={task}
+                    handleOpen={this.handleOpen}
+                    handleEdit={this.editTaskWork}
+                />
+            );
+            continguts.push(
+                <div>
+                    <TextField
+                        disabled={true}
+                        defaultValue={this.props.task.estimated_hours}
+                        floatingLabelText="Hores estimades"
+                    />
+                    <TextField style={{paddingLeft: 10}}
+                        disabled={true}
+                        defaultValue={this.props.task.dedicated_hours}
+                        floatingLabelText="Hores dedicades"
+                    />
+                </div>
+            );
+        }
         return(
-            <div style={estils.container}>
-                <MainPaper>
-                    <table style={estils.mainTable}>
-                        <tbody>
-                        <tr>
-                            <td style={estils.titol}>
-                                {
-                                    this.props.task ?
-                                        <div>{this.props.task.description}</div>
-                                        :
-                                        <div>Tasca</div>
-                                }
-                            </td>
-                            <td>
-                                <div style={estils.botoNou}>
-                                    <FlatButton
-                                        label="Nou workdone"
-                                        primary={true}
-                                        icon={<FontIcon className="material-icons">note_add</FontIcon>}
-                                        onTouchTap={this.newTaskWork}
-                                    />
-                                </div>
-                            </td>
-                        </tr>
-                        {
-                            this.props.task ?
-                                <div>
-                                    <tr>
-                                        <td colSpan="2" style={estils.subtitol}>
-                                            <div>Projecte: {this.props.task.project}</div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colSpan="2" style={estils.subtitol}>
-                                            <TextField
-                                                disabled={true}
-                                                defaultValue={this.props.task.estimated_hours}
-                                                floatingLabelText="Hores estimades"
-                                            />
-                                        </td>
-                                        <td colSpan="2" style={estils.subtitol}>
-                                            <TextField
-                                                disabled={true}
-                                                defaultValue={this.props.task.dedicated_hours}
-                                                floatingLabelText="Hores dedicades"
-                                            />
-                                        </td>
-                                    </tr>
-                                </div>
-                                :
-                                <div></div>
-                        }
-                        </tbody>
-                    </table>
-                    {
-                        !this.props.task ?
-                            <div style={{padding: 30}}>
-                                No s'ha seleccionat cap tasca.
-                            </div>
-                            :
-                            <TaskWorkList taskWorks={this.props.taskWorks.data.taskWorks} />
-                    }
-                </MainPaper>
-            </div>
+            <MainView
+                title={title}
+                breadcrumb={project}
+                contents={continguts}
+                table={<List columns={cols} tableBody={tableContents}/>}
+            />
         )
-    }
-
-    newTaskWork() {
-        console.log("New workdone called");
     }
 }
