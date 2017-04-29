cf2ca0246e19515075a49c08a238f20779acfc75
Axel Simón <asimon@gisce.net>
2017-04-29 18:05:31 +0200

    New users path

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import { App } from './containers/App';
 import Projects from './components/ProjectsView';
 import Tasks from './components/TasksView';
 import Task from './components/TaskView';
+import Users from './components/UserView';
 
 export default (
     <Route path="/" component={App}>
         <Route path="projects" component={Projects} />
         <Route path="tasks" component={Tasks} />
         <Route path="task" component={Task} />
+        <Route path="users" component={Users} />
     </Route>
 );
