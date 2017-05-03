f8c7fc01236dfe0bfa421c6edbe19398b077e3af
Axel Simón <asimon@gisce.net>
2017-05-03 18:41:52 +0200

    User view development

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -2,27 +2,52 @@ import React, { Component } from 'react';
 import { TOKEN } from '../constants/index';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as actionCreators from '../actions/users';
-import User from './User'
+import * as tasksCreators from '../actions/tasks';
+import * as userCreators from '../actions/users'
+import Task from './Task'
 import List from './List'
 import MainView from './MainView'
+import Avatar from 'material-ui/Avatar'
+import FlatButton from 'material-ui/FlatButton'
+import FontIcon from 'material-ui/FontIcon'
 
 function mapStateToProps(state) {
     return {
-        data: state.users,
+        tasks: state.tasks.data,
+        users: state.users.data,
         token: null,
-        loaded: state.users.loaded,
-        isFetching: state.users.isFetching,
+        tasksLoaded: state.tasks.loaded,
+        userLoaded: state.users.loaded,
+        isFetchingTasks: state.tasks.isFetching,
+        isFetchingUsers: state.users.isFetching,
         message_text: state.users.message_text,
     };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch);
+    return bindActionCreators(Object.assign({}, tasksCreators, userCreators), dispatch);
 }
 
+const style = {
+    userAvatar: {
+        float: 'left'
+    },
+    userInfo: {
+        float: 'right',
+        marginLeft: '10px'
+    },
+    userName: {
+        marginTop: '30px',
+    },
+    userLogin: {
+        marginTop: '10px',
+        fontStyle: 'italic',
+        fontSize: '14pt'
+    }
+};
+
 @connect(mapStateToProps, mapDispatchToProps)
-export default class ProjectsView extends Component {
+export default class UserView extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -35,32 +60,74 @@ export default class ProjectsView extends Component {
     }
 
     fetchData(initial = true) {
-        this.props.fetchUsers(TOKEN, initial);
+        let taskFilter = "&filter=[('user_id','='," + this.props.params.userId + "),('state','in',['open','pending'])]";
+        this.props.fetchTasks(TOKEN, null, taskFilter, false);
+        let userFilter = "&filter=[('id','='," + this.props.params.userId + ")]";
+        this.props.fetchUsers(TOKEN, [], userFilter, initial);
     }
 
     render() {
-        let tableContents = "No hi ha usuaris per mostrar.";
+        let isFetching = this.props.isFetchingTasks || this.props.isFetchingUsers;
+        let tableContents = "No existeix cap usuari amb ID " + this.props.params.userId + ".";
         let cols = [
-            "Avatar",
-            "Nom",
-            "Usuari",
-            "Ultima connexió"
+            "Tasca",
+            "Projecte",
+            "Estat",
+            "Data inici"
         ];
-        if(this.props.loaded){
-            let users = this.props.data.data.users;
-            tableContents = users.map(user =>
-                <User
-                    key={user.id}
-                    user={user}
-                />)
+        let tasks = [];
+        let userdata = [];
+        let buttons = [];
+        let user = null;
+        if(this.props.tasksLoaded && this.props.userLoaded && !isFetching){
+            let tasks = this.props.tasks.tasks;
+            tableContents = tasks.map(task =>
+                <Task
+                    key={task.id}
+                    task={task}
+                    userMode={true}
+                />);
+            user = this.props.users.users[0];
+            userdata.push(
+                <div key="-1">
+                    <div style={style.userAvatar}>
+                        <Avatar
+                            src={user.avatar}
+                            size={120}
+                            style={style}
+                        />
+                    </div>
+                    <div style={style.userInfo}>
+                        <div style={style.userName}>
+                            {user.name}
+                        </div>
+                        <div style={style.userLogin}>
+                            {user.login}
+                        </div>
+                    </div>
+                </div>
+            );
+            buttons.push(
+                <FlatButton
+                    key="-2"
+                    label="Totes les tasques"
+                    primary={true}
+                    icon={<FontIcon className="material-icons">view_list</FontIcon>}
+                    onTouchTap={console.log("Veure totes les tasques")}
+                />
+            );
         }
         return(
             <MainView
-                original_ids={[]}
-                model="users"
-                title="Usuaris"
-                fetching={this.props.isFetching}
+                original_ids={tasks}
+                model="tasks"
+                title={userdata}
+                fetching={isFetching}
+                buttons={buttons}
                 refresh={() => this.fetchData(false)}
+                filters="disabled"
+                newButton="disabled"
+                contents="Tasques on l'usuari treballa actualment:"
                 table={<List columns={cols} tableBody={tableContents}/>}
             />
         )
