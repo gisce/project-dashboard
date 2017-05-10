b3eb3cf25c7a054a7fd4db7d704f420c560e07e2
Axel Simón <asimon@gisce.net>
2017-05-10 19:05:38 +0200

    Loading indicator component added

---
+++ src/components/LoadingIndicator/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+
+export default class LoadingIndicator extends Component {
+    render(){
+        return (
+            <div className="loading">
+                <CircularProgress size={100} thickness={4}/>
+            </div>
+        )
+    }
+}
