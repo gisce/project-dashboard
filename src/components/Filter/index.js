b4e6d0edda71361e7dc33c08583b660bfbb972bc
Axel Simón <asimon@gisce.net>
2017-05-24 20:18:32 +0200

    Filter component skeleton added

---
+++ src/components/Filter/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Filter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>Filter</div>
+        )
+    }
+}
