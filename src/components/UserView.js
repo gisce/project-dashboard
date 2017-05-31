f7e4c9221ef31e7a517172bf9068513fd7236da0
Axel Simón <asimon@gisce.net>
2017-05-31 19:34:46 +0200

    Filter refactor in userview

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -62,7 +62,10 @@ export default class UserView extends Component {
 
 
     fetchData(initial = true) {
-        let taskFilter = "&filter=[('user_id','='," + this.props.params.userId + "),('state','in',['open','pending'])]";
+        let taskFilter = [
+            ["user_id", "=", parseInt(this.props.params.userId, 10)],
+            ["state", "in", ["open", "pending"]]
+        ];
         this.props.fetchTasks(TOKEN, taskFilter, false, false);
         this.props.fetchUsers(TOKEN, this.props.params.userId, true, initial);
     }
