5f41b7a7970864b919c4e2aa1a190f3f3ac3a281
Axel Simón <asimon@gisce.net>
2017-05-31 19:32:21 +0200

    Tasks action refactor

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,8 +1,8 @@
-import {FETCH_TASKS_REQUEST, RECEIVE_TASKS, SET_ACTIVE_TASK} from '../constants'
-import {redirectToRoute, define_token} from '../utils/http_functions'
-import {parseJSON, parseTasks, parseProjects} from '../utils/misc'
-import {setActiveProject, receiveProjects} from './projects'
-import axios  from 'axios'
+import {FETCH_TASKS_REQUEST, RECEIVE_TASKS, SET_ACTIVE_TASK} from '../constants';
+import {define_token} from '../utils/http_functions';
+import {setActiveProject, receiveProjects} from './projects';
+import axios  from 'axios';
+import { Project, Task } from '../models/model';
 
 export function fetchTasksRequest(initial) {
     const message = (initial)?null:"Refreshing tasks list";
@@ -38,38 +38,33 @@ export function setActiveTask(active_task) {
 export function fetchTasks(token, filter, project_id, initial = false) {
     return (dispatch) => {
         dispatch(fetchTasksRequest(initial));
-        let uri = "http://localhost:5000/project.task?" +
-            "schema=name,project_id.id, project_id.name,user_id.name,total_hours,remaining_hours,planned_hours," +
-            "effective_hours,priority,state,work_ids,delay_hours" + filter;
         if(!axios.defaults.headers.common['Authorization']){
             define_token(token);
         }
-        axios.get(uri)
-            .then(parseJSON)
-            .then(response => {
-                dispatch(receiveTasks(parseTasks(response, false), initial));
-            })
-            .then(response => {
-                /*
-                * If there's not any active_project, it's necessary to fetch the project to retrieve extra project info.
-                * It may be possible with a single api call. TODO
-                * */
-                if(project_id){
-                    let filter = "&filter=[('id','='," + project_id + ")]";
-                    axios.get("http://localhost:5000/project.project?schema=name,tasks,manager.name,state" + filter)
-                        .then(parseJSON)
-                        .then(response => {
-                            let projects = parseProjects(response, false);
-                            dispatch(receiveProjects(projects, initial));
-                            dispatch(setActiveProject(projects[0]));
-                        })
-                        .catch(error => {
-                            console.log("API ERROR", error);
-                        });
+        let model = new Task();
+        model.search(filter, {
+            transformResponse: [function (data) {
+                let newData = JSON.parse(data);
+                let results = [];
+                if (newData.n_items > 0) {
+                    results = model.parse(newData, false);
                 }
-            })
-            .catch(error => {
-                console.log("API ERROR", error);
-            });
+                dispatch(receiveTasks(results, initial));
+                if(project_id){
+                    let project = new Project();
+                    project.search([["id", "=", parseInt(project_id, 10)]], {
+                        transformResponse: [function (data) {
+                            let newData = JSON.parse(data);
+                            let results = [];
+                            if (newData.n_items > 0){
+                                results = project.parse(newData, false);
+                            }
+                            dispatch(receiveProjects(results, initial));
+                            dispatch(setActiveProject(results[0]));
+                        }]
+                    });
+                }
+            }]
+        });
     }
 }
