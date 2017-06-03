62387f03ad6f16456b7fa869c48b59bedbfa284d
Axel Simón <asimon@gisce.net>
2017-06-03 20:23:00 +0200

    ADD_FILTER added to reducer file

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,13 +1,19 @@
-import { SET_FILTERS } from '../constants';
+import { SET_FILTERS, ADD_FILTER } from '../constants';
 import { createReducer } from '../utils/misc';
 
 const initialState = {
-    filters: {}
+    filters: {},
+    search_function: null
 };
 
 export default createReducer(initialState, {
     [SET_FILTERS]: (state, payload) =>
         Object.assign({}, state, {
             filters: payload.filters,
-        })
+            search_function: payload.search_function
+        }),
+    [ADD_FILTER]: (state, payload) =>
+    Object.assign({}, state, {
+        filters: Object.assign({}, state.filters, payload.filter)
+    })
 });
