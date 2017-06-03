e91707c991554f0f5489f341d975bf32e60488f8
Axel Simón <asimon@gisce.net>
2017-06-03 20:16:48 +0200

    AddFilter added to filter action redux file

--- src/actions/filter.js
+++ src/actions/filter.js
@@ -1,10 +1,20 @@
-import {SET_FILTERS} from '../constants'
+import { SET_FILTERS, ADD_FILTER } from '../constants'
 
-export function setFilters(filters) {
+export function setFilters(filters, search_function) {
     return {
         type: SET_FILTERS,
         payload: {
-            filters
+            filters,
+            search_function
+        }
+    }
+}
+
+export function addFilter(filter) {
+    return {
+        type: ADD_FILTER,
+        payload: {
+            filter
         }
     }
 }
