8e2237aea333c961a0dba563967f9c1dc8543324
Axel Simón <asimon@gisce.net>
2017-06-13 19:26:04 +0200

    UI reducers now can handle dialogs

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,9 +1,10 @@
-import { UI_OPEN_MENU, UI_CLOSE_MENU, UI_OPEN_FILTER_BUTTON_MENU, UI_CLOSE_FILTER_BUTTON_MENU } from '../constants';
+import { UI_OPEN_MENU, UI_CLOSE_MENU, UI_OPEN_FILTER_BUTTON_MENU, UI_CLOSE_FILTER_BUTTON_MENU, UI_OPEN_DIALOG, UI_CLOSE_DIALOG } from '../constants';
 import { createReducer } from '../utils/misc';
 
 const initialState = {
     menu_open: false,
-    filter_menu_open: false
+    filter_menu_open: false,
+    dialog_open: false
 };
 
 export default createReducer(initialState, {
@@ -22,5 +23,17 @@ export default createReducer(initialState, {
     [UI_CLOSE_FILTER_BUTTON_MENU]: (state, payload) =>
         Object.assign({}, state, {
             filter_menu_open: payload.filter_menu_open,
-        })
+        }),
+    [UI_OPEN_DIALOG]: (state, payload) =>
+        Object.assign({}, state, {
+            isFetching: true,
+            message_text: payload.message,
+            dialog_open: payload.dialog_open
+        }),
+    [UI_CLOSE_DIALOG]: (state, payload) =>
+        Object.assign({}, state, {
+            isFetching: true,
+            message_text: payload.message,
+            dialog_open: payload.dialog_open
+        }),
 });
