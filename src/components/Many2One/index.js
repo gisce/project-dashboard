478ae543f50d5a8b108366ed99781406fe2f07a7
Axel Simón <asimon@gisce.net>
2017-06-14 19:18:05 +0200

    Many2One component bugfix

--- src/components/Many2One/index.js
+++ src/components/Many2One/index.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
 import { TOKEN } from '../../constants/index';
 
-let searchText = "";
-let dataSource = [];
-
 export default class Many2One extends Component {
     constructor(props){
         super(props);
         this.handleUpdateInput = this.handleUpdateInput.bind(this);
         this.handleNewRequest = this.handleNewRequest.bind(this);
-        searchText = "";
+        this.filter = this.filter.bind(this);
+        this.searchText = "";
+        this.dataSource = [];
+    }
+
+    filter(searchText, key){
+        return key.indexOf(searchText) !== -1
     }
 
     handleUpdateInput(textToSearch){
-        searchText = textToSearch;
-        this.props.searchFunction(TOKEN, textToSearch, "name", false, false)
+        this.searchText = textToSearch;
+        this.props.searchFunction(TOKEN, textToSearch, "name")
     };
 
     handleNewRequest(item){
@@ -33,19 +36,25 @@ export default class Many2One extends Component {
     };
 
     render(){
-        if(this.props.source && searchText.length >= 1) {
+        if(this.props.source && this.searchText.length >= 1) {
             for (let i = 0; i < this.props.source.length; i++) {
-                dataSource.push(this.props.source[i].name);
+                if(this.dataSource.indexOf(this.props.source[i].name) === -1) {
+                    this.dataSource.push(this.props.source[i].name);
+                }
             }
+        }
+        else{
+            this.dataSource = [];
         }
         return (
             <AutoComplete
-                hintText={this.props.label}
-                searchText={searchText}
+                floatingLabelText={this.props.label}
+                hintText="Escriu per cercar"
+                searchText={this.searchText}
                 onUpdateInput={this.handleUpdateInput}
                 onNewRequest={this.handleNewRequest}
-                dataSource={dataSource}
-                filter={(searchText, key) => (key.indexOf(searchText) !== -1)}
+                dataSource={this.dataSource}
+                filter={this.filter}
                 openOnFocus={true}
             />
         )
