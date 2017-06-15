d79c6be99641b86bcb4aeb6fb71e6c772e50ed61
Axel Simón <asimon@gisce.net>
2017-06-15 19:43:36 +0200

    CreateProject reducer added

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -1,11 +1,12 @@
-import { RECEIVE_PROJECTS, FETCH_PROJECTS_REQUEST, SET_ACTIVE_PROJECT } from '../constants';
+import { RECEIVE_PROJECTS, FETCH_PROJECTS_REQUEST, SET_ACTIVE_PROJECT, CREATE_PROJECT } from '../constants';
 import { createReducer } from '../utils/misc';
 
 const initialState = {
     data: null,
     isFetching: false,
     loaded: false,
-    active_project: null
+    active_project: null,
+    message_text: null
 };
 
 export default createReducer(initialState, {
@@ -24,5 +25,9 @@ export default createReducer(initialState, {
     [SET_ACTIVE_PROJECT]: (state, payload) =>
         Object.assign({}, state, {
             active_project: payload.active_project
+        }),
+    [CREATE_PROJECT]: (state, payload) =>
+        Object.assign({}, state, {
+            message_text: payload.message_text
         })
 });
