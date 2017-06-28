7e2de4b5c697733e8fdf30872225b7df016bfdd3
Axel Simón <asimon@gisce.net>
2017-06-28 18:39:29 +0200

    Style modifications for better screen adaptation in CompaniesView

--- src/components/CompaniesView.js
+++ src/components/CompaniesView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MainPaper from './MainPaper';
 import {browserHistory} from 'react-router';
 import { TOKEN } from '../constants/index';
 import { connect } from 'react-redux';
@@ -37,9 +38,9 @@ function mapDispatchToProps(dispatch) {
 }
 
 const cols = {
-    "Nom": "name",
-    "Ciutat": "city",
-    "País": "country"
+    "Nom": ["name", {width: "130px"}],
+    "Ciutat": ["city", {width: "130px"}],
+    "País": ["country", {width: "130px"}]
 };
 
 let activeFilters = [];
@@ -79,60 +80,62 @@ export default class CompaniesView extends Component {
             companies = this.props.data.data.companies;
         }
         return(
-            <div>
-                <div className="leftContainer">
-                    {
-                        !this.props.isFetching && (
-                            <div>
-                                <div className="title">
-                                    Empreses
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
-                <div className="rightContainer">
-                    {
-                        !this.props.isFetching && (
-                            <div className="upperButtons">
-                                <FilterButton
-                                    filters={this.props.filters}
-                                    setter={this.props.setFilters}
-                                    adder={this.props.addFilter}
-                                    activeFilters={activeFilters}
-                                />
-                                <RefreshButton
-                                    refresh={() => this.fetchData(false)}
-                                />
-                            </div>
-                        )
-                    }
-                    <div className="searchBox">
+            <div className="mainPaperContainer">
+                <MainPaper>
+                    <div className="leftContainer">
                         {
-                            !this.props.isFetching &&
-                            <SearchBox
-                                searchFunction={this.props.searchCompanies}
-                                field="name"
+                            !this.props.isFetching && (
+                                <div>
+                                    <div className="title">
+                                        Empreses
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className="rightContainer">
+                        {
+                            !this.props.isFetching && (
+                                <div className="upperButtons">
+                                    <FilterButton
+                                        filters={this.props.filters}
+                                        setter={this.props.setFilters}
+                                        adder={this.props.addFilter}
+                                        activeFilters={activeFilters}
+                                    />
+                                    <RefreshButton
+                                        refresh={() => this.fetchData(false)}
+                                    />
+                                </div>
+                            )
+                        }
+                        <div className="searchBox">
+                            {
+                                !this.props.isFetching &&
+                                <SearchBox
+                                    searchFunction={this.props.searchCompanies}
+                                    field="name"
+                                />
+                            }
+                        </div>
+                    </div>
+                    <div className="filters">
+                        {activeFilters}
+                    </div>
+                    <div className="tableContainer" style={{paddingTop: 50 }}>
+                        {
+                            !this.props.loaded || this.props.isFetching ?
+                                <LoadingIndicator/>
+                            :
+                            <SmartTable
+                                handleClick={this.handleClick}
+                                handleUpdate={this.props.receiveCompanies}
+                                columns={cols}
+                                data={companies}
                             />
                         }
                     </div>
-                </div>
-                <div className="filters">
-                    {activeFilters}
-                </div>
-                <div className="tableContainer" style={{paddingTop: 50 }}>
-                    {
-                        !this.props.loaded || this.props.isFetching ?
-                            <LoadingIndicator/>
-                        :
-                        <SmartTable
-                            handleClick={this.handleClick}
-                            handleUpdate={this.props.receiveCompanies}
-                            columns={cols}
-                            data={companies}
-                        />
-                    }
-                </div>
+                </MainPaper>
             </div>
         )
     }
