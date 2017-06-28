530e5688af3cd4fb81bb9ff6facce0524c4ec621
Axel Simón <asimon@gisce.net>
2017-06-28 18:37:59 +0200

    MainPaper added on not found view

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import MainPaper from './MainPaper';
 
 export default class NotFound extends Component {
     render() {
         return(
-            <div>
-                <div className="contents">
-                    <h2 style={{textAlign: "center"}}>Error 404: no s'han trobat els continguts sol·licitats.</h2>
-                    <br/>
-                </div>
+            <div className="mainPaperContainer">
+                <MainPaper>
+                    <div className="contents">
+                        <h2 style={{textAlign: "center"}}>Error 404: no s'han trobat els continguts sol·licitats.</h2>
+                        <br/>
+                    </div>
+                </MainPaper>
             </div>
         )
     }
