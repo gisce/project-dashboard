ae86ab3193dc1ececd459304679b4c8208d68e67
Axel Simón <asimon@gisce.net>
2017-06-29 20:22:24 +0200

    LoginView now triggers real AUTH requests

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,12 +4,41 @@ import TextField from 'material-ui/TextField';
 import Avatar from 'material-ui/Avatar';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as authCreators from '../actions/auth';
 
 const style = {
     margin: 12,
 };
 
+let fields = {};
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+        isAuthenticating: state.auth.isAuthenticating,
+        token: state.auth.token
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(authCreators, dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.login = this.login.bind(this);
+    }
+
+    login(){
+        if("user" in fields && "password" in fields) {
+            this.props.basicAuth(fields["user"], fields["password"]);
+        }
+    }
+
     render() {
         return(
             <div className="mainPaperContainer_login">
@@ -24,15 +53,17 @@ export default class Login extends Component {
                     <TextField
                         ref="user"
                         floatingLabelText="Usuari"
+                        onChange={e => fields["user"] = e.target.value}
                     />
                     <br/>
                     <TextField
                         ref="user"
                         floatingLabelText="Contrasenya"
                         type="password"
+                        onChange={e => fields["password"] = e.target.value}
                     />
                     <br/>
-                    <RaisedButton label="Iniciar sessió" primary={true} style={style} />
+                    <RaisedButton label="Iniciar sessió" primary={true} style={style} onTouchTap={this.login} />
                 </MainPaper>
             </div>
         )
