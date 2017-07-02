465f868ca9e03e6d9bfd99683e2f35f17a3c8bec
Axel Simón <asimon@gisce.net>
2017-07-02 19:50:02 +0200

    UI improvements in login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,12 @@ import FontIcon from 'material-ui/FontIcon';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as authCreators from '../actions/auth';
+import * as uiCreators from '../actions/ui';
+import LoadingIndicator from './LoadingIndicator';
 
 const style = {
     margin: 12,
+    marginTop: 20
 };
 
 let fields = {};
@@ -18,12 +21,13 @@ function mapStateToProps(state) {
     return {
         isAuthenticated: state.auth.isAuthenticated,
         isAuthenticating: state.auth.isAuthenticating,
-        token: state.auth.token
+        token: state.auth.token,
+        error: state.auth.error
     };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(authCreators, dispatch);
+    return bindActionCreators(Object.assign({}, authCreators, uiCreators), dispatch);
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
@@ -43,17 +47,24 @@ export default class Login extends Component {
         return(
             <div className="mainPaperContainer_login">
                 <MainPaper>
-                    <Avatar
-                        style={{marginTop: "20px"}}
-                        backgroundColor="#00897B"
-                        size={80}
-                        icon={<FontIcon className="material-icons">lock_outline</FontIcon>}
-                    />
+                    {!this.props.isAuthenticating ?
+                        <Avatar
+                            style={{marginTop: "20px"}}
+                            backgroundColor="#00897B"
+                            size={80}
+                            icon={<FontIcon className="material-icons">lock_outline</FontIcon>}
+                        />
+                        :
+                        <LoadingIndicator
+                            size={60}
+                        />
+                    }
                     <br/>
                     <TextField
                         ref="user"
                         floatingLabelText="Usuari"
                         onChange={e => fields["user"] = e.target.value}
+                        disabled={this.props.isAuthenticating}
                     />
                     <br/>
                     <TextField
@@ -61,6 +72,8 @@ export default class Login extends Component {
                         floatingLabelText="Contrasenya"
                         type="password"
                         onChange={e => fields["password"] = e.target.value}
+                        disabled={this.props.isAuthenticating}
+                        errorText={this.props.error}
                     />
                     <br/>
                     <RaisedButton label="Iniciar sessió" primary={true} style={style} onTouchTap={this.login} />
