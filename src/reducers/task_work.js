2419ee3b0679af75ca5ee59803e2bcd120a0eeb5
Axel Simón <asimon@gisce.net>
2017-07-10 20:55:30 +0200

    create task work is now asyncronous

--- src/reducers/task_work.js
+++ src/reducers/task_work.js
@@ -1,10 +1,11 @@
-import { RECEIVE_TASK_WORK, FETCH_TASK_WORK_REQUEST, CREATE_TASK_WORK, PATCH_TASK_WORK_REQUEST, PATCH_TASK_WORK_RESPONSE, DELETE_TASK_WORK_REQUEST, DELETE_TASK_WORK_RESPONSE } from '../constants';
+import { RECEIVE_TASK_WORK, FETCH_TASK_WORK_REQUEST, CREATE_TASK_WORK_REQUEST, CREATE_TASK_WORK_RESPONSE, PATCH_TASK_WORK_REQUEST, PATCH_TASK_WORK_RESPONSE, DELETE_TASK_WORK_REQUEST, DELETE_TASK_WORK_RESPONSE } from '../constants';
 import { createReducer } from '../utils/misc';
 
 const initialState = {
     data: null,
     isFetching: false,
-    loaded: false
+    loaded: false,
+    message_text: null
 };
 
 export default createReducer(initialState, {
@@ -20,11 +21,15 @@ export default createReducer(initialState, {
             isFetching: true,
             message_text: payload.message,
         }),
-    [CREATE_TASK_WORK]: (state, payload) =>
+    [CREATE_TASK_WORK_REQUEST]: (state, payload) =>
         Object.assign({}, state, {
             message_text: payload.message,
         }),
     [PATCH_TASK_WORK_REQUEST]: (state, payload) =>
+        Object.assign({}, state, {
+            message_text: payload.message,
+        }),
+    [PATCH_TASK_WORK_RESPONSE]: (state, payload) =>
         Object.assign({}, state, {
             message_text: payload.message,
         }),
