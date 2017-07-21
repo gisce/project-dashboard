f16e30a663bc8fe7cb5e4a4ed141b52ffa1b248f
Axel Simón <asimon@gisce.net>
2017-07-21 20:03:59 +0200

    Newtask view error displaying enhanced

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -10,13 +10,15 @@ import * as breadcrumbCreators from '../actions/breadcrumb';
 import LinkButton from './LinkButton';
 import Many2One from './Many2One';
 import TextField from 'material-ui/TextField';
+import {timeFormat} from '../utils/misc';
 
 function mapStateToProps(state) {
     return {
         token: state.auth.token,
         projects: state.projects,
         users: state.users,
-        active_project: state.projects.active_project
+        active_project: state.projects.active_project,
+        fields_errors: state.ui.fields_errors
     };
 }
 
@@ -25,6 +27,7 @@ function mapDispatchToProps(dispatch) {
 }
 
 let fields = {};
+let error = [];
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class NewTask extends Component {
@@ -32,6 +35,7 @@ export default class NewTask extends Component {
         super(props);
         this.createTaskCall = this.createTaskCall.bind(this);
         this.reload = this.reload.bind(this);
+        this.handleTimeFormat = this.handleTimeFormat.bind(this);
         fields = {};
     }
 
@@ -48,18 +52,49 @@ export default class NewTask extends Component {
         }
     }
 
-    createTaskCall(){
-        if(fields.hasOwnProperty("name") && fields.hasOwnProperty("project_id") && fields.hasOwnProperty("planned_hours")) {
-            this.props.createTask(this.props.token, fields, this.reload);
-            this.props.openToastRequest("Tasca creada");
+    handleTimeFormat(time){
+        const res = timeFormat(time, 'float');
+        error = [];
+        if(res[0] === 'ok'){
+            delete fields["time_error"];
+            this.updateFields("planned_hours", res[1]);
         }
         else{
-            this.props.openDialogRequest("Atenció", "És necessari escriure el resum de la tasca, el projecte al qual pertany i les hores planificades.");
+            delete fields["planned_hours"];
+            this.updateFields("time_error", res[1]);
+            error.push(res[1]);
         }
     }
 
+    createTaskCall(){
+        let errors_dict = {};
+        if(fields.hasOwnProperty("name") && fields.hasOwnProperty("project_id") && fields.hasOwnProperty("planned_hours") && !fields.hasOwnProperty("time_error")) {
+            delete fields["time_error"];
+            this.props.createTask(this.props.token, fields, this.reload);
+            this.props.openToastRequest("Tasca creada");
+        }
+        if(!fields.hasOwnProperty("name")){
+            errors_dict['resum_tasca'] = 'Camp obligatori';
+        }
+        if(!fields.hasOwnProperty("project_id")){
+            errors_dict['projecte'] = 'Camp obligatori';
+        }
+        if(!fields.hasOwnProperty("planned_hours")){
+            errors_dict['time_error'] = 'Camp obligatori';
+        }
+        if(error.length > 0){
+            errors_dict['time_error'] =  error[0];
+        }
+        this.props.setFieldsErrors(errors_dict);
+    }
+
     updateFields(field, value){
-        fields[field] = value;
+        if(String(value).length > 0 && String(value) !== "0"){
+            fields[field] = value;
+        }
+        else{
+            delete fields[field];
+        }
     }
 
     render() {
@@ -84,11 +119,25 @@ export default class NewTask extends Component {
                         <div className="leftColumn">
                             <TextField
                                 floatingLabelText="Resum de la tasca"
-                                onChange={e => fields["name"] = e.target.value}
+                                onChange={e => this.updateFields("name", e.target.value)}
+                                errorText={
+                                    (this.props.fields_errors && "resum_tasca" in this.props.fields_errors) ? (
+                                        this.props.fields_errors['resum_tasca']
+                                    )
+                                        :
+                                    ''
+                                }
                             />
                             <TextField
                                 floatingLabelText="Hores estimades"
-                                onChange={e => fields["planned_hours"] = parseFloat(e.target.value)}
+                                onChange={e => this.handleTimeFormat(e.target.value)}
+                                errorText={
+                                    (this.props.fields_errors && "time_error" in this.props.fields_errors) ? (
+                                        this.props.fields_errors['time_error']
+                                    )
+                                        :
+                                    ''
+                                }
                             />
                         </div>
                         <div className="rightColumn">
@@ -99,6 +148,13 @@ export default class NewTask extends Component {
                                 updateFields={this.updateFields}
                                 searchFunction={this.props.searchProjects}
                                 defaultValue={defaultValue}
+                                errorText={
+                                    (this.props.fields_errors && "projecte" in this.props.fields_errors) ? (
+                                        this.props.fields_errors['projecte']
+                                    )
+                                        :
+                                    ''
+                                }
                             />
                             <br/>
                             <Many2One
