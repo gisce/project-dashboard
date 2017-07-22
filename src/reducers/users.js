da0d99a5025b2ccd6b728b664a68ec5e4d6ec421
Axel Simón <asimon@gisce.net>
2017-07-22 13:35:16 +0200

    Show all user tasks flag reducer added

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,11 @@
-import { RECEIVE_USERS, FETCH_USERS_REQUEST } from '../constants';
+import { RECEIVE_USERS, FETCH_USERS_REQUEST,SHOW_ALL_USER_TASKS_FLAG } from '../constants';
 import { createReducer } from '../utils/misc';
 
 const initialState = {
     data: null,
     isFetching: false,
-    loaded: false
+    loaded: false,
+    allTasks: false
 };
 
 export default createReducer(initialState, {
@@ -18,6 +19,11 @@ export default createReducer(initialState, {
     [FETCH_USERS_REQUEST]: (state, payload) =>
         Object.assign({}, state, {
             isFetching: true,
+            message_text: payload.message,
+        }),
+    [SHOW_ALL_USER_TASKS_FLAG]: (state, payload) =>
+        Object.assign({}, state, {
+            allTasks: payload.flag,
             message_text: payload.message,
         })
 });
