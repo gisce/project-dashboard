0c6cafcdcea9d270024475b59acb280043fc9d4b
Axel Simón <asimon@gisce.net>
2017-08-02 22:01:52 +0200

    Dashboard view now prints real charts

--- src/components/DashboardView.js
+++ src/components/DashboardView.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import Dashboard from 'react-dazzle';
 import MainPaper from './MainPaper';
+import LoadingIndicator from './LoadingIndicator';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as dashboardCreators from '../actions/dashboard';
 
 // Widgets of the dashboard.
-import LineChart from './widgets/LineChart';
-import DoughnutChart from './widgets/DoughnutChart';
-import RadarChart from './widgets/RadarChart';
+import StartedTasksCountLineChart from './widgets/StartedTasksCountLineChart';
+import UserTasksDoughnutChart from './widgets/UserTasksDoughnutChart';
+import TaskStatusRadarChart from './widgets/TaskStatusRadarChart';
 // Default styles.
 import 'react-dazzle/lib/style/style.css';
 
 function mapStateToProps(state) {
     return {
-        dazzle_state: state.dashboard.dazzle_state
+        token: state.auth.token,
+        dazzle_state: state.dashboard.dazzle_state,
+        isFetching: state.dashboard.isFetching
     };
 }
 
@@ -33,14 +36,16 @@ export default class DashboardView extends Component {
             // Widgets that are available in the dashboard
             widgets: {
                 prova1: {
-                    type: LineChart,
-                    title: 'Primer gràfic',
+                    type: StartedTasksCountLineChart,
+                    title: 'Tasques iniciades el darrer any',
                 },
                 prova2: {
-                    type: DoughnutChart,
+                    type: UserTasksDoughnutChart,
+                    title: "Tasques per usuari"
                 },
                 prova3: {
-                    type: RadarChart,
+                    type: TaskStatusRadarChart,
+                    title: "Estat de les tasques"
                 },
             },
             // Layout of the dashboard
@@ -53,15 +58,18 @@ export default class DashboardView extends Component {
                         className: 'widgets',
                         widgets: [{key: 'prova2'}],
                     },
-                    {
-                        className: 'widgets',
-                        widgets: [{key: 'prova3'}],
-                    }],
+                        {
+                            className: 'widgets',
+                            widgets: [{key: 'prova3'}],
+                        }],
                 }],
             },
             editable: false,
         };
         this.props.setDazzleState(dazzleState);
+        this.props.getTasksStatus(this.props.token);
+        this.props.getUserTasksCount(this.props.token);
+        this.props.getStartedTasksCount(this.props.token);
     }
 
     componentDidMount(){
@@ -73,13 +81,21 @@ export default class DashboardView extends Component {
             <div className="mainPaperContainer">
                 <MainPaper>
                     <div className="contents">
-                        <div className="dashboard">
-                            {
-                                Object.keys(this.props.dazzle_state).length > 0 &&
-                                    <Dashboard  widgets={this.props.dazzle_state.widgets} layout={this.props.dazzle_state.layout}  />
-                            }
-                        </div>
-                        <div style={{clear: 'both'}}></div>
+                        {
+                            this.props.isFetching ?
+                                <LoadingIndicator/>
+                                :
+                                <div>
+                                    <div className="dashboard">
+                                        {
+                                            Object.keys(this.props.dazzle_state).length > 0 &&
+                                            <Dashboard widgets={this.props.dazzle_state.widgets}
+                                                       layout={this.props.dazzle_state.layout}/>
+                                        }
+                                    </div>
+                                    <div style={{clear: 'both'}}></div>
+                                </div>
+                        }
                     </div>
                 </MainPaper>
             </div>
