bb1552a002cd78392cefe5e98977379e87d8cf02
Axel Simón <asimon@gisce.net>
2017-08-02 22:02:38 +0200

    Tasks status radar chart now shows real info

--- src/components/widgets/TaskStatusRadarChart.js
+++ src/components/widgets/TaskStatusRadarChart.js
@@ -1,65 +1,66 @@
 import React, { Component } from 'react';
 import { Radar } from 'react-chartjs';
-import { getRandomInt } from '../../utils/misc';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as dashboardCreators from '../../actions/dashboard';
 
-class RadarChart extends Component {
+function mapStateToProps(state) {
+    return {
+        token: state.auth.token,
+        tasks_status: state.dashboard.tasks_status
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(Object.assign({}, dashboardCreators), dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+class TaskStatusRadarChart extends Component {
     constructor() {
         super();
-
-        this.state = {
-            data: {
-                labels: ['10', '20', '30', '40', '50', '60', '70'],
-                datasets: [
-                    {
-                        label: 'Singal',
-                        fillColor: '#F1E7E5',
-                        strokeColor: '#E8575A',
-                        pointColor: '#E8575A',
-                        pointStrokeColor: '#fff',
-                        pointHighlightFill: '#ff',
-                        pointHighlightStroke: 'rgba(220,220,220,1)',
-                        data: [10, 55, 69, 45, 87, 68, 74],
-                    },
-                    {
-                        label: 'Disturbance',
-                        fillColor: 'rgba(151,187,205,0.2)',
-                        strokeColor: 'rgba(151,187,205,1)',
-                        pointColor: 'rgba(151,187,205,1)',
-                        pointStrokeColor: '#fff',
-                        pointHighlightFill: '#fff',
-                        pointHighlightStroke: 'rgba(151,187,205,1)',
-                        data: [10, 55, 69, 45, 87, 68, 74],
-                    },
-                ],
-            },
-        };
+        this.getTasksData = this.getTasksData.bind(this);
     }
 
-    componentDidMount() {
-        const refreshIntervalId = setInterval(() => {
-            this.state.data.datasets[0].data.shift();
-            this.state.data.datasets[0].data.push(getRandomInt(0, 90));
-
-            this.state.data.datasets[1].data.shift();
-            this.state.data.datasets[1].data.push(getRandomInt(0, 90));
-            this.setState({
-                data: this.state.data,
-                refreshIntervalId,
-            });
-        }, 2000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.state.refreshIntervalId);
+    getTasksData(){
+        let res = [];
+        res.push(this.props.tasks_status["open"]);
+        res.push(this.props.tasks_status["draft"]);
+        res.push(this.props.tasks_status["pending"]);
+        res.push(this.props.tasks_status["cancelled"]);
+        res.push(this.props.tasks_status["done"]);
+        return res;
     }
 
     render() {
+        if(Object.keys(this.props.tasks_status).length > 0){
+            const data = this.getTasksData();
+            this.state = {
+                data: {
+                    labels: ['En progrés', 'Esborrany', 'Pendent', 'Cancel·lat', 'Realitzat'],
+                    datasets: [
+                        {
+                            label: 'Tasques',
+                            fillColor: '#80CBC4',
+                            strokeColor: '#00897B',
+                            pointColor: '#004D40',
+                            pointStrokeColor: '#fff',
+                            pointHighlightFill: '#ff',
+                            pointHighlightStroke: 'rgba(220,220,220,1)',
+                            data: data,
+                        }
+                    ],
+                },
+            };
+        }
         return (
-            <div>
-              <Radar data={this.state.data} options={{responsive: true, animationSteps: 300 }} height="200" width="500"/>
-            </div>
+            Object.keys(this.props.tasks_status).length > 0 && (
+                <div>
+                    <Radar data={this.state.data} options={{responsive: true, animationSteps: 300}} height="200" width="300"/>
+                </div>
+            )
         );
     }
 }
 
-export default RadarChart;
+export default TaskStatusRadarChart;
